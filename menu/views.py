970703970e9df5aaf39f90d433b0009d1e66f9c3
Mgirijashree <mgirija.shree@gmail.com>
2026-07-24 11:26:57 +0530

    Fix food menu ratings display

--- menu/views.py
+++ menu/views.py
@@ -1,27 +1,22 @@
 from django.shortcuts import render
 from django.db.models import Avg
-
 from .models import FoodItem
 
 
 def food_list(request):
 
-    foods = (
-        FoodItem.objects
-        .select_related("restaurant", "category")
-        .prefetch_related(
-            "reviews",
-            "reviews__customer",
-        )
-        .annotate(
-            average_rating=Avg("reviews__rating"),
-        )
+    foods = FoodItem.objects.select_related(
+        "restaurant"
+    ).prefetch_related(
+        "reviews"
+    ).annotate(
+        average_rating=Avg("reviews__rating")
     )
 
     return render(
         request,
         "menu/food_list.html",
         {
-            "foods": foods,
-        },
+            "foods": foods
+        }
     )
